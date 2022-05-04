b66a80e9c55bbd7a7e30dec4500689b52c2013aa
Gustavo Saiani <gustavo@poe.ma>
2022-05-04 19:43:13 +0200

    Add place to show picked color

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 window.addEventListener("load", function() {
+  initializeVariationButtons();
+  initializeColorPickers();
+});
+
+function initializeVariationButtons() {
   const buttons = document.querySelectorAll("button");
 
   for (const button of buttons) {
     button.addEventListener("click", applyVariations);
   }
-});
+}
+
+function initializeColorPickers() {
+  const pickers = document.querySelectorAll("input[type='color']");
+
+  for (const picker of pickers) {
+    picker.addEventListener("change", onColorPick);
+  }
+}
 
 function applyVariations(event) {
   const colorContainer = event.target.closest(".color-picker-container");
 
-  const pickedColor = colorContainer.querySelector("input[type='color']").value;
+  const pickedColor = colorContainer.querySelector(".primary-color").style
+    .backgroundColor;
 
-  paintVariations(pickedColor, colorContainer);
+  if (pickedColor) {
+    paintVariations(pickedColor, colorContainer);
+  }
 }
 
 function paintVariations(color, colorContainer) {
@@ -36,4 +52,15 @@ function defineVariations(color) {
     .hex();
 
   return [darker, lighter];
+}
+
+function onColorPick(event) {
+  const picker = event.target;
+  const color = picker.value;
+
+  const primaryColorContainer = picker
+    .closest("div")
+    .querySelector(".primary-color");
+
+  primaryColorContainer.style.backgroundColor = color;
 }
