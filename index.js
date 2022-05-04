613b9d136cfda7c4527671c69653f80c5682bc5a
Gustavo Saiani <gustavo@poe.ma>
2022-05-04 22:10:07 +0200

    Fill up colors if 1 is set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
   initializeVariationButtons();
   initializeColorPickers();
+  initializeFillMissingColors();
 });
 
 function initializeVariationButtons() {
@@ -63,4 +64,39 @@ function onColorPick(event) {
     .querySelector(".primary-color");
 
   primaryColorContainer.style.backgroundColor = color;
+}
+
+function initializeFillMissingColors() {
+  const button = document.querySelector(".fill-missing-colors");
+  button.addEventListener("click", onClickFillMissingColors);
+}
+
+function onClickFillMissingColors(event) {
+  const [
+    firstColorContainer,
+    secondColorContainer,
+    thirdColorContainer,
+    fourthColorContainer,
+    fifthColorContainer,
+    sixthColorContainer,
+    seventhColorContainer,
+    eighthColorContainer
+  ] = document.querySelectorAll(".primary-color");
+  const firstColor = firstColorContainer.style.backgroundColor;
+
+  const secondColor = chroma(firstColor).set("hsl.h", "-45");
+  const thirdColor = chroma(firstColor).set("hsl.h", "-90");
+  const fourthColor = chroma(firstColor).set("hsl.h", "-135");
+  const fifthColor = chroma(firstColor).set("hsl.h", "-180");
+  const sixthColor = chroma(firstColor).set("hsl.h", "-225");
+  const seventhColor = chroma(firstColor).set("hsl.h", "-270");
+  const eighthColor = chroma(firstColor).set("hsl.h", "-315");
+
+  secondColorContainer.style.backgroundColor = secondColor;
+  thirdColorContainer.style.backgroundColor = thirdColor;
+  fourthColorContainer.style.backgroundColor = fourthColor;
+  fifthColorContainer.style.backgroundColor = fifthColor;
+  sixthColorContainer.style.backgroundColor = sixthColor;
+  seventhColorContainer.style.backgroundColor = seventhColor;
+  eighthColorContainer.style.backgroundColor = eighthColor;
 }
