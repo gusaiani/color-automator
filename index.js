b9e17af640abeb36f1c9d10fbc853f9e46f69955
Gustavo Saiani <gustavo@poe.ma>
2022-05-05 13:42:44 +0200

    Abstract color setting

--- index.js
+++ index.js
@@ -103,34 +103,27 @@ function detectFilledPrimaryColors() {
 }
 
 function fillPrimaryColorsFromOnePrimary() {
-  const [
-    firstColorContainer,
-    secondColorContainer,
-    thirdColorContainer,
-    fourthColorContainer,
-    fifthColorContainer,
-    sixthColorContainer,
-    seventhColorContainer,
-    eighthColorContainer
-  ] = document.querySelectorAll(".primary-color");
+  const primaryColorDivs = document.querySelectorAll(".primary-color");
 
-  const firstColor = firstColorContainer.style.backgroundColor;
+  const newPrimaryColors = [...primaryColorDivs].reduce(
+    (acc, primaryColorDiv, index) => {
+      const firstColor = primaryColorDivs[0].style.backgroundColor;
+      const newColor =
+        index === 0
+          ? firstColor
+          : chroma(firstColor)
+              .set("hsl.h", String(index * -45))
+              .hex();
+      acc.push(newColor);
 
-  const secondColor = chroma(firstColor).set("hsl.h", "-45");
-  const thirdColor = chroma(firstColor).set("hsl.h", "-90");
-  const fourthColor = chroma(firstColor).set("hsl.h", "-135");
-  const fifthColor = chroma(firstColor).set("hsl.h", "-180");
-  const sixthColor = chroma(firstColor).set("hsl.h", "-225");
-  const seventhColor = chroma(firstColor).set("hsl.h", "-270");
-  const eighthColor = chroma(firstColor).set("hsl.h", "-315");
+      return acc;
+    },
+    []
+  );
 
-  secondColorContainer.style.backgroundColor = secondColor;
-  thirdColorContainer.style.backgroundColor = thirdColor;
-  fourthColorContainer.style.backgroundColor = fourthColor;
-  fifthColorContainer.style.backgroundColor = fifthColor;
-  sixthColorContainer.style.backgroundColor = sixthColor;
-  seventhColorContainer.style.backgroundColor = seventhColor;
-  eighthColorContainer.style.backgroundColor = eighthColor;
+  primaryColorDivs.forEach((primaryColorDiv, index) => {
+    primaryColorDiv.style.backgroundColor = newPrimaryColors[index];
+  });
 }
 
 function fillAllVariations() {
